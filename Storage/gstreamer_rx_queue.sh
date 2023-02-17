175e87911526c91343a0cc0d9ab3c84e0179f566
Pete A <peter.allenm@gmail.com>
2023-02-17 20:30:46 +0000

    Mostly working openhd

--- Storage/gstreamer_rx_queue.sh
+++ Storage/gstreamer_rx_queue.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+number=0
 
-gst-launch-1.0 -v udpsrc port=5600 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! h264parse ! v4l2slh264dec ! queue max-size-buffers=1 leaky=downstream ! kmssink sync=0 plane-id=31 plane-properties=s,zpos=3
+fname=OpenHD.mkv
+
+while [ -e "/storage/$fname" ]; do
+    number=$(( ++number ))
+    fname=OpenHD-$number.mkv
+done
+
+printf 'Will use "%s" as filename\n' "/storage/$fname"
+echo "should be using /storage/$fname as filename for gstreamer output" >/storage/GStreamerFile.txt
+gst-launch-1.0 -v udpsrc port=5600 ! "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! h264parse ! tee name=t \
+    t. ! v4l2slh264dec ! queue max-size-buffers=1 leaky=downstream ! kmssink sync=0 plane-id=31 plane-properties=s,zpos=3 \
+    t. ! queue ! matroskamux ! filesink location="/storage/$fname" >& /storage/GStreamerLog.txt
+
